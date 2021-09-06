d9d84a4011db2c3ef9419a92b15b4f7ada5c1de2
Indra <indraberga@inbox.lv>
2021-09-06 14:12:16 +0300

    My app HW1

---
+++ My1stapp/My1stapp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace My1stapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
